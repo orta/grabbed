b93306837a1cba7667dd68f668119f19c19390d3
/orta <orta.therox@gmail.com>
2010-08-06 09:13:17 +0100

    wrong import

--- main.m
+++
@@ -1,8 +0,0 @@
-#import <UIKit/UIKit.h>
-
-int main(int argc, char *argv[]) {
-	NSAutoreleasePool *pool = [NSAutoreleasePool new];
-	UIApplicationMain(argc, argv, nil, @"AppController");
-	[pool release];
-	return 0;
-}
